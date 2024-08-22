d3599c466db82fe45b2eebc94ebe6e43202df620
Amanda Martins <amanda_nelva07@hotmail.com>
2024-08-21 22:07:46 -0300

    inserção dos arquivos csv no BD

--- Trabalho_Banco_De_Dados/BackEnd/server.py
+++ Trabalho_Banco_De_Dados/BackEnd/server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import pymysql
 import sqlalchemy
 import pandas as pd
+import csv
 from sqlalchemy import create_engine
 
 app = Flask(__name__, static_url_path='/static')
@@ -14,41 +15,87 @@ app = Flask(__name__, static_url_path='/static')
 #   database="Showdomilhao"
 # )
 
-mydb = pymysql.connect(
+# Conectar ao banco de dados
+conn = pymysql.connect(
   host="localhost",
   user="root",
   password="Minas@0202",
-  database="Showdomilhao"
+  database="Showdomilhao",
+  charset="utf8mb4"
 )
 
-# engine = create_engine('mysql://root:Minas@0202@host/showdomilhao')
-# df = pd.read_csv('meu_arquivo.csv')
-# df.to_sql('nome_da_tabela', con=engine, if_exists='append', index=False)
+# Criar um cursor para executar as queries
+cursor = conn.cursor()
+
+# Nome do arquivo CSV
+pergunta_csv = 'C:/Users/AmandaeLuiz04/Downloads/Trabalho_Banco_De_Dados/Trabalho_Banco_De_Dados/BackEnd/pergunta.csv'
+alternativa_csv = 'C:/Users/AmandaeLuiz04/Downloads/Trabalho_Banco_De_Dados/Trabalho_Banco_De_Dados/BackEnd/alternativa.csv'
+
+# Abrir o arquivo CSV
+with open(pergunta_csv, 'r', encoding='utf-8') as csvfile:
+    # Criar um leitor CSV
+    csvreader = csv.reader(csvfile)
+    # Ignorar a primeira linha se ela contiver os cabeçalhos
+    next(csvreader, None)
+
+    # Inserir os dados na tabela
+    for row in csvreader:
+        # Adaptar a query SQL de acordo com a estrutura da sua tabela
+        sql = "INSERT INTO pergunta (enunciado) VALUES (%s)"
+        cursor.execute(sql, row)
+
+# Abrir o arquivo CSV
+with open(alternativa_csv, 'r', encoding='utf-8') as csvfile:
+    # Criar um leitor CSV
+    csvreader = csv.reader(csvfile)
+    # Ignorar a primeira linha se ela contiver os cabeçalhos
+    next(csvreader, None)
+
+    # Inserir os dados na tabela
+    for row in csvreader:
+        
+        idpergunta = int(row[0]) 
+        conteudo = row[1]
+        alternativacorreta = row[2]
+        # Adaptar a query SQL de acordo com a estrutura da sua tabela
+        sql = "INSERT INTO alternativa (idpergunta, conteudo, alternativacorreta) VALUES (%d, %s, %s)"
+        cursor.execute(sql, (idpergunta, conteudo, alternativacorreta))       
+
+# Confirmar as alterações no banco de dados
+conn.commit()
+
+# Fechar a conexão com o banco de dados
+conn.close()
+
+
+""""engine = create_engine('mysql://root:Minas@0202@host/showdomilhao')
+df = pd.read_csv('pergunta.csv')
+df.to_sql('pergunta', con=engine, if_exists='append', index=False)"""
 
 # Realizar login
-@app.route('/', methods=['GET', 'POST'])
+"""@app.route('/', methods=['GET', 'POST'])
 def root():
-    return render_template('index.html')
-# def indexlogin():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         senha = request.form['senha']
+  return render_template('index.html')
+  def indexlogin():
+     if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
 
-#         # Consultar o banco de dados
-#         mycursor = mydb.cursor()
-#         sql = "SELECT * FROM jogador WHERE email = %s"
-#         val = (email,)
-#         mycursor.execute(sql, val)
-#         jogador = mycursor.fetchone()
+         # Consultar o banco de dados
+         mycursor = mydb.cursor()
+         sql = "SELECT * FROM jogador WHERE email = %s"
+         val = (email,)
+         mycursor.execute(sql, val)
+         jogador = mycursor.fetchone()
 
-#         if jogador and jogador[3] == senha:
-#              return redirect(url_for('menu'))  # Redireciona para a página inicial onde o jogador escolhe se quer jogar
-#                                               # ou ver as estatíticas
-#         else:
-#             return render_template('index.html', error='Usuário ou senha inválidos.')
+         if jogador and jogador[3] == senha:
+              return redirect(url_for('menu'))  # Redireciona para a página inicial onde o jogador escolhe se quer jogar
+                                               # ou ver as estatíticas
+         else:
+             return render_template('index.html', error='Usuário ou senha inválidos.')
 
-#     PATH = '../assets/Html/index.html'
-#     return render_template(PATH)
+     PATH = '../assets/Html/index.html'
+     return render_template(PATH)
 
 
 # Rota para salvar dados do formulário
@@ -72,7 +119,7 @@ def salvar():
 
   return "Cadastro bem sucedido!"
 
-app.run()
+app.run()"""
 
 
 
